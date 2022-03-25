5caf21247ac043cb069f574390394363c6a8735f
G-Ilian <gbiel.boza@gmail.com>
2022-03-25 13:27:05 -0300

    Criada uma classe que modela o fornecedor, conforme pedido do desafio

---
+++ Desafio_Backend_4Intelligence/Fornecedores/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+'''
+Classe de modelagem do desafio, assim como requisitado.
+Todos os campos tem os tamanhos e os tipos pedidos.
+'''
+#Classe de modelagem do problema
+class Fornecedores(models.Model):
+    id=models.CharField(max_length=16,primary_key=True)
+    name=models.CharField(max_length=140)
+    company=models.CharField(max_length=50)
+    created_at=models.DateTimeField()
+    amount_products=models.IntegerField()
+
+    def __str__(self):
+        return self.name
