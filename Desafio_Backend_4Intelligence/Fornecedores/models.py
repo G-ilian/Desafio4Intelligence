69cf7be9f22f87b1cfa5f3ae154f4a3dbf8be9cb
G-Ilian <gbiel.boza@gmail.com>
2022-03-26 22:54:02 -0300

    Mudanças para entrar de acordo com o PEP 8

--- Desafio_Backend_4Intelligence/Fornecedores/models.py
+++ Desafio_Backend_4Intelligence/Fornecedores/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
-
 '''
 Classe de modelagem do desafio, assim como requisitado.
 Todos os campos tem os tamanhos e os tipos pedidos.
 '''
-#Classe de modelagem do problema
+
+
+# Classe de modelagem do problema
+
 class Fornecedores(models.Model):
-    id=models.CharField(max_length=16,primary_key=True)
-    name=models.CharField(max_length=140)
-    company=models.CharField(max_length=50)
-    created_at=models.DateTimeField()
-    amount_products=models.IntegerField()
+    id = models.CharField(max_length=16, primary_key=True)
+    name = models.CharField(max_length=140)
+    company = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    amount_products = models.IntegerField()
 
     def __str__(self):
         return self.name
