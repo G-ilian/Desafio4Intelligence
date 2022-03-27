ac9481b8182723d51374613a59f3a4a8eadc6896
G-Ilian <gbiel.boza@gmail.com>
2022-03-27 14:38:54 -0300

    Adicionado as classes para realizar as operações de CRUD, removido a classe FornecedoresViewSet ela fazia todas as operações de maneira abstraída, ou seja, sob o mesmo endpoint, diferente do requisitado para o desafio

--- Desafio_Backend_4Intelligence/Fornecedores/api/viewsets.py
+++ Desafio_Backend_4Intelligence/Fornecedores/api/viewsets.py
@@ -1,55 +1,47 @@
-from functools import partial
-
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from rest_framework import permissions
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateAPIView,RetrieveDestroyAPIView
 from Fornecedores.models import Fornecedores
-from rest_framework.response import Response
-from .serializers import FornecedoresSerializer, FornecedorUpdateSerializer
-
-'''
-Configurando uma viewset, que será utilizada para a url.
-Através do ModelViewSet e suas facilidades é possível realizar um CRUD para um model qualquer
-'''
-
-
-class FornecedoresViewSet(ModelViewSet):
-    queryset = Fornecedores.objects.all()
-    serializer_class = FornecedoresSerializer
+from .serializers import FornecedoresSerializer
 
 
 # Classe responsável por realizar a criação de fornecedores, herda de uma generic view que permite realizar a operação
 
 class FornecedoresCreate(CreateAPIView):
     queryset = Fornecedores.objects.all()
+
     serializer_class = FornecedoresSerializer
 
-    def create(self, request, *args, **kwargs):
-        return super(FornecedoresCreate, self).create(request, *args, **kwargs)
 
+'''
+Classe responsável por realizar a listagem/leitura de fornecedores, herda de uma generic view que permite realizar 
+a operação 
+'''
 
-# Classe responsável por realizar a listagem/leitura de fornecedores, herda de uma generic view que permite realizar a operação
 
 class FornecedoresRead(ListAPIView):
-    queryset = Fornecedores.objects.all()
-    serializer_class = FornecedoresSerializer
+    queryset = Fornecedores.objects.all()# Acessando todos os objetos de fornecedores que foram adicionados
 
-    def list(self, request, *args, **kwargs):
-        # Note the use of `get_queryset()` instead of `self.queryset`
-        return super(FornecedoresRead, self).list(request, *args, **kwargs)
+    serializer_class = FornecedoresSerializer  # Serializando os objetocs, ou seja, passando de python para JSON
+
 
 
 # Classe responsável por realizar o update de fornecedores, herda de uma generic view que permite realizar a operação
 
 class FornecedoresUpdate(RetrieveUpdateAPIView):
-    queryset = Fornecedores.objects.all()
-    serializer_class = FornecedoresSerializer
-    lookup_field = 'id'
+
+    queryset = Fornecedores.objects.all() # Acessando todos os objetos de fornecedores que foram adicionados
+
+    serializer_class = FornecedoresSerializer # Serializando os objetos, ou seja, passando de python para JSON
+
+    lookup_field = 'id' # Sobescrevendo a chave que é usada como padrão pelo DRF
 
 
 # Classe responsável por realizar o delete de fornecedores, herda de uma generic view que permite realizar a operação
 
-class FornecedoresDelete(DestroyAPIView):
-    queryset = Fornecedores.objects.all()
-    serializer_class = FornecedoresSerializer
-    lookup_field = 'id'
+class FornecedoresDelete(RetrieveDestroyAPIView):
+
+    queryset = Fornecedores.objects.all() # Acessando todos os objetos de fornecedores que foram adicionados
+
+    serializer_class = FornecedoresSerializer # Serializando os objetocs, ou seja, passando de python para JSON
+
+    lookup_field = 'id' # Sobescrevendo a chave que é usada como padrão pelo DRF
